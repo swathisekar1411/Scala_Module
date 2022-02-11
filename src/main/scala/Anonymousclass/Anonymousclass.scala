b70c1ecdc8b4653f56810869f573d6d0a0094d63
Swathi Gnanasekar <88793179+swathisekar1411@users.noreply.github.com>
2022-02-11 11:30:47 +0530

    Update Anonymousclass.scala

--- src/main/scala/Anonymousclass/Anonymousclass.scala
+++ src/main/scala/Anonymousclass/Anonymousclass.scala
@@ -2,13 +2,14 @@ package Anonymousclass
 
 object Anonymousclass extends App{
   abstract class Animal {
-
+     
     def eat: Unit
+    
   }
 
   // class Anonymousclass$$anon$1 extends animal {
   // override def eat: Unit = println("ahahah")
-  //anon class
+  //anonymousclass class
   val funanimal: Animal = new Animal {
     override def eat: Unit = println("ahahah")
     eat
